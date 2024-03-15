6b0f8a210de1a0bb400a38c58da6abcb9e5c0144
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2024-03-15 14:51:28 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Card from './components/card'
+import Score from './components/score'
+import './App.css'
+
+function App() {
+
+  const [cards, setCards] = useState([]);
+  const [score, setScore] = useState(0);
+
+  function randomizeCards() {
+    
+  }
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
